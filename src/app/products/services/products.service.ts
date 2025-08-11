cedd6649a23b6672a3bcebb4930a690349626ac1
José Alonso <mail@josealonso.dev>
2025-08-11 13:55:20 -0600

    feat: configure environment settings with base URL for API integration

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -4,16 +4,16 @@ import { ListProductDto } from '@products/interfaces/list-product.dto';
 import { ListProductResponse } from '@products/interfaces/list-product.response.interface';
 import { ProductsMapper } from '@products/interfaces/products.mapper';
 import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+const baseUrl = environment.baseUrl;
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   private http = inject(HttpClient);
-  private baseUrl = 'http://localhost:3000/api';
 
   getProducts(): Observable<ListProductDto> {
-    const response = this.http.get<ListProductResponse>(
-      `${this.baseUrl}/products`
-    );
+    const response = this.http.get<ListProductResponse>(`${baseUrl}/products`);
     return response.pipe(map((res) => ProductsMapper.toListProductDto(res)));
   }
 }
