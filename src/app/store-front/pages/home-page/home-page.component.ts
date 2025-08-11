b86bc317f0bf4c7e45fed5333aaf69c6db1cc530
José Alonso <mail@josealonso.dev>
2025-08-11 13:44:52 -0600

    feat: enhance HomePageComponent with products service integration and resource management

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
   selector: 'app-home-page',
   imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => {
+      return this.productsService.getProducts();
+    },
+  });
+}
