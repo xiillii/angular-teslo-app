b6a7282a521f642dab07092a15805eaddc95f3c5
José Alonso <mail@josealonso.dev>
2025-08-11 00:27:30 -0600

    feat: update routerLink binding in front-navbar component for improved routing

--- src/app/store-front/components/front-navbar/front-navbar.component.ts
+++ src/app/store-front/components/front-navbar/front-navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'front-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './front-navbar.component.html',
 })
 export class FrontNavbarComponent {}
