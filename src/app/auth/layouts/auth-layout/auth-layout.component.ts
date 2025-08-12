554f968118c0883d8a08bb99cfbed3050e4a6a5c
José Alonso <mail@josealonso.dev>
2025-08-12 01:29:05 -0600

    feat: add authentication layout component with template

---
+++ src/app/auth/layouts/auth-layout/auth-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '../../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K';
+
+@Component({
+  selector: 'app-auth-layout',
+  imports: [RouterOutlet],
+  templateUrl: './auth-layout.component.html',
+})
+export class AuthLayoutComponent {}
