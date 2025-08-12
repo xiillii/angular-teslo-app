32866ceed319e70ba4b067fb336487babf83be95
José Alonso <mail@josealonso.dev>
2025-08-12 01:37:31 -0600

    feat: implement authentication routing with login and register pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+    // TODO: add AuthGuard
+  },
+  {
     path: '',
     loadChildren: () => import('./store-front/store-front.routes'),
   },
