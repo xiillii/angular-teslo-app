433bb6e186e26ea0fb1dab57173c3aed4d9f0e34
José Alonso <mail@josealonso.dev>
2025-08-11 23:16:04 -0600

    feat: enhance pagination component with router integration and active page tracking

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,13 +1,23 @@
-import { Component, computed, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  input,
+  linkedSignal,
+  signal,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagination.component.html',
 })
 export class PaginationComponent {
   pages = input<number>(0);
   currentPage = input<number>(1);
+
+  // linkedSignal is recommended for signals that are initializated with another signal
+  activePage = linkedSignal(this.currentPage);
 
   getPagesList = computed(() => {
     return Array.from({ length: this.pages() }, (_, index) => index + 1);
