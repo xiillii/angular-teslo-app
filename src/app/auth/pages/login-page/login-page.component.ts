c4243f4ad135aff964e61d4ce5c8971a1f5f11ee
José Alonso <mail@josealonso.dev>
2025-08-12 02:24:05 -0600

    feat: enhance login functionality with reactive forms and validation

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { FormUtils } from '@shared/utils/form-utils';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  formUtils = FormUtils;
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
+
+  loginForm = this.fb.group({
+    email: [
+      '',
+      [Validators.required, Validators.pattern(this.formUtils.emailPattern)],
+    ],
+    password: ['', [Validators.required, Validators.minLength(3)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 3000);
+      return;
+    }
+
+    console.log('Form submitted:', this.loginForm.value);
+  }
+}
---
+++ src/app/shared/utils/form-utils.ts
@@ -0,0 +1,112 @@
+import {
+  AbstractControl,
+  FormArray,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
+
+async function sleep() {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(true);
+    }, 2500)
+  );
+}
+
+export class FormUtils {
+  // Expresiones regulares para validaciones
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
+  private static getTextErrors(errors: ValidationErrors): string | null {
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return 'El mínimo de caracteres es ' + errors[key].requiredLength;
+        case 'min':
+          return 'El valor mínimo es ' + errors[key].min;
+        case 'email':
+          return 'El formato del email es inválido';
+        case 'pattern':
+          return 'El formato es inválido';
+        case 'emailTaken':
+          return 'El email ya está en uso';
+        case 'noStrider':
+          return 'El valor no puede ser "strider"';
+      }
+    }
+    return null;
+  }
+
+  static isValidField(formGroup: FormGroup, fieldName: string): boolean | null {
+    return !(
+      formGroup.controls[fieldName].errors &&
+      formGroup.controls[fieldName].touched
+    );
+  }
+
+  static getFieldError(formGroup: FormGroup, fieldName: string): string | null {
+    if (!formGroup.controls[fieldName].errors) {
+      return null;
+    }
+
+    const errors = formGroup.controls[fieldName].errors ?? {};
+
+    return this.getTextErrors(errors);
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return !(
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  static getFieldErrorInArray(
+    formArray: FormArray,
+    index: number
+  ): string | null {
+    if (formArray.controls.length === 0) {
+      return null;
+    }
+    const errors = formArray.controls[index].errors ?? {};
+
+    return this.getTextErrors(errors);
+  }
+
+  static isFieldOneEqualsFieldTwo(fieldOne: string, fieldTwo: string) {
+    return (formGroup: AbstractControl) => {
+      const value1 = formGroup.get(fieldOne)?.value;
+      const value2 = formGroup.get(fieldTwo)?.value;
+      return value1 === value2 ? null : { notMatching: true };
+    };
+  }
+
+  static async checkingServerResponse(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> {
+    console.log('Checking server response...');
+    await sleep();
+
+    const formValue = control.value;
+
+    if (formValue === 'hola@mundo.com') {
+      return {
+        emailTaken: true,
+      };
+    }
+
+    return null;
+  }
+
+  static noStrider(control: AbstractControl): ValidationErrors | null {
+    const formValue = control.value;
+
+    if (formValue === 'strider') {
+      return { noStrider: true };
+    }
+    return null;
+  }
+}
