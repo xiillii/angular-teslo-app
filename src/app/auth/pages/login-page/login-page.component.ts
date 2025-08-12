c39b0f4b8df7c0d7e030e1dc5c3932bfea93fdca
José Alonso <mail@josealonso.dev>
2025-08-12 01:57:48 -0600

    feat: implement login form with email and password fields

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent { }
+export class LoginPageComponent {}
