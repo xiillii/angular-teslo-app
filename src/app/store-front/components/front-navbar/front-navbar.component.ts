f19edef28a99453a153fcf02c40ac2e1ac71e74a
José Alonso <mail@josealonso.dev>
2025-08-11 23:54:57 -0600

    fix: correct routerLinkActive class name in front-navbar component for proper styling style: update daisyui theme from business to night in styles.css

--- src/app/store-front/components/front-navbar/front-navbar.component.ts
+++ src/app/store-front/components/front-navbar/front-navbar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'front-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './front-navbar.component.html',
 })
 export class FrontNavbarComponent {}
