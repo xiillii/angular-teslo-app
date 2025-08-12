8220c7e13a77cc796c3d3cf6953edddc0330b67a
José Alonso <mail@josealonso.dev>
2025-08-11 22:27:11 -0600

    feat: enhance gender page component with route parameter handling and dynamic gender display

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
   imports: [],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent { }
+export class GenderPageComponent {
+  route = inject(ActivatedRoute);
+
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+}
