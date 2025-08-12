58beb4b573c2cddb19605763e9fe6161b7f78ade
José Alonso <mail@josealonso.dev>
2025-08-11 21:57:42 -0600

    feat: implement product carousel with swiper integration and dynamic image handling

--- src/app/products/components/product-carousel/product-carousel.component.ts
+++ src/app/products/components/product-carousel/product-carousel.component.ts
@@ -1,10 +1,56 @@
-import { Component, input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  input,
+  viewChild,
+} from '@angular/core';
+
+import Swipper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 
 @Component({
   selector: 'app-product-carousel',
-  imports: [],
+  imports: [ProductImagePipe],
   templateUrl: './product-carousel.component.html',
+  styles: `
+    .swiper {
+      width: 100%;
+      height: 500px;
+    }
+  `,
 })
-export class ProductCarouselComponent {
+export class ProductCarouselComponent implements AfterViewInit {
   images = input.required<string[] | null | undefined>();
+  swiperDiv = viewChild.required<ElementRef>('swiperDiv');
+
+  ngAfterViewInit() {
+    const element = this.swiperDiv().nativeElement;
+
+    if (!element) return;
+
+    const swiper = new Swipper(element, {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+
+      modules: [Navigation, Pagination],
+
+      // if we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
 }
