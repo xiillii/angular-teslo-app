9f4cf45cec4607809f352180a8d0667eb2ad0ca6
José Alonso <mail@josealonso.dev>
2025-08-13 17:24:27 -0600

    feat: enhance products admin page with pagination component integration and logic

--- src/app/admin-darshboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-darshboard/pages/products-admin-page/products-admin-page.component.ts
@@ -2,16 +2,21 @@ import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductsTableComponentComponent } from '@products/components/productsTableComponent/productsTableComponent.component';
 import { ProductsService } from '@products/services/products.service';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/services/pagination.service';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [ProductsTableComponentComponent],
+  imports: [ProductsTableComponentComponent, PaginationComponent],
   templateUrl: './products-admin-page.component.html',
 })
 export class ProductsAdminPageComponent {
   productService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
   productsResource = rxResource({
-    loader: () => this.productService.getProducts({}),
+    request: () => ({ page: this.paginationService.currentPage() - 1 }),
+    loader: ({ request }) =>
+      this.productService.getProducts({ offset: request.page * 10, limit: 10 }),
   });
 }
