0921130733bdd9d0e2e459cd106656972888e342
José Alonso <mail@josealonso.dev>
2025-08-12 18:11:36 -0600

    feat: implement login functionality with DTOs for user authentication

--- src/app/auth/interfaces/auth.mapper.ts
+++ src/app/auth/interfaces/auth.mapper.ts
@@ -1,3 +1,5 @@
+import { LoginDto } from './login.dto';
+import { LoginResponse } from './login.response.interface';
 import { UserDto } from './user.dto';
 import { User } from './user.response.interface';
 
@@ -9,6 +11,13 @@ export class AuthMapper {
       fullName: user.fullName,
       isActive: user.isActive,
       roles: user.roles,
+    };
+  }
+
+  static toLoginDto(loginResponse: LoginResponse): LoginDto {
+    return {
+      user: AuthMapper.toUserDto(loginResponse.user),
+      token: loginResponse.token,
     };
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
+import { AuthMapper } from '@auth/interfaces/auth.mapper';
+import { LoginDto } from '@auth/interfaces/login.dto';
+import { LoginResponse } from '@auth/interfaces/login.response.interface';
 import { UserDto } from '@auth/interfaces/user.dto';
+import { map, Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
+const baseUrl = environment.baseUrl;
 type AuthStatus = 'checking' | 'authenticated' | 'unauthenticated';
 
 @Injectable({ providedIn: 'root' })
@@ -22,4 +28,19 @@ export class AuthService {
 
   user = computed<UserDto | null>(() => this._user());
   token = computed<string | null>(() => this._token());
+
+  login(email: string, password: string): Observable<LoginDto> {
+    return this.http
+      .post<LoginResponse>(`${baseUrl}/auth/login`, { email, password })
+      .pipe(
+        map((response) => AuthMapper.toLoginDto(response)),
+        tap((dto) => {
+          this._user.set(dto.user);
+          this._token.set(dto.token);
+          this._authStatus.set('authenticated');
+
+          localStorage.setItem('token', dto.token);
+        })
+      );
+  }
 }
