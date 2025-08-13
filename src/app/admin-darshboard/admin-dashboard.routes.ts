5c3a441c5e191e07f6536de69935f433cae56690
José Alonso <mail@josealonso.dev>
2025-08-12 22:55:49 -0600

    feat: add admin dashboard layout and product management pages with routing

--- src/app/admin-darshboard/admin-dashboard.routes.ts
+++ src/app/admin-darshboard/admin-dashboard.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { AdminDashboardLayoutComponent } from './layouts/admin-dashboard-layout/admin-dashboard-layout.component';
+import { ProductsAdminPageComponent } from './pages/products-admin-page/products-admin-page.component';
+import { ProductAdminPageComponent } from './pages/product-admin-page/product-admin-page.component';
 
-export const adminDashboardRoutes: Routes = [];
+export const adminDashboardRoutes: Routes = [
+  {
+    path: '',
+    component: AdminDashboardLayoutComponent,
+    children: [
+      {
+        path: 'products',
+        component: ProductsAdminPageComponent,
+      },
+      {
+        path: 'products/:id',
+        component: ProductAdminPageComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'products',
+  },
+];
+
+export default adminDashboardRoutes;
---
+++ src/app/admin-darshboard/layouts/admin-dashboard-layout/admin-dashboard-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '../../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K';
+
+@Component({
+  selector: 'app-admin-dashboard-layout',
+  imports: [RouterOutlet],
+  templateUrl: './admin-dashboard-layout.component.html',
+})
+export class AdminDashboardLayoutComponent {}
