6a62dfbb724ef585dfaa6a117c28d208e2983714
José Alonso <mail@josealonso.dev>
2025-08-12 20:59:34 -0600

    feat: add NotAuthenticatedGuard for route protection against authenticated users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from '@auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes'),
-    // TODO: add AuthGuard
+    canMatch: [NotAuthenticatedGuard],
   },
   {
     path: '',
