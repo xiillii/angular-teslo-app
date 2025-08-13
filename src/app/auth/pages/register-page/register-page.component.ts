bb12b0d40fc3085e869183f93a5a90a47d59b4b4
José Alonso <mail@josealonso.dev>
2025-08-12 22:12:55 -0600

    feat: implement registration form with validation and error handling

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
+import { FormUtils } from '@shared/utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  formUtils = FormUtils;
+  fb = inject(FormBuilder);
+  hasError = signal<boolean>(false);
+  isPosting = signal<boolean>(false);
+  router = inject(Router);
+  authService = inject(AuthService);
+
+  registerForm = this.fb.group(
+    {
+      fullname: ['', [Validators.required, Validators.minLength(1)]],
+      email: [
+        '',
+        [Validators.required, Validators.pattern(this.formUtils.emailPattern)],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repeatPassword: ['', [Validators.required, Validators.minLength(6)]],
+    },
+    {
+      validators: this.formUtils.isFieldOneEqualsFieldTwo(
+        'password',
+        'repeatPassword'
+      ),
+    }
+  );
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      this.hasError.set(true);
+      this.registerForm.markAllAsTouched();
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 5000);
+      return;
+    }
+  }
+}
